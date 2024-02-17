2d94b5ca7546853914fe17ee63426b90ecf44ca1
lux-ferre <milne_christopher@hotmail.com>
2024-02-17 17:15:53 +0000

    background colour applied to full body
    Removed from #content
    "bg-*" classes used to correct navbar element
    jQuery used to toggle navbar bg class for light mode

--- app/static/assets/main/colour-mode.js
+++ app/static/assets/main/colour-mode.js
@@ -1,3 +1,6 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
 	document.documentElement.setAttribute("data-bs-theme", "light")
+	const navbar = $(".navbar")
+	navbar.removeClass("bg-dark")
+	navbar.addClass("bg-light")
 }
