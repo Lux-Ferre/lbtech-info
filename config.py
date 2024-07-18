5b980eeeff4ae60fb77cdf4ebb353df766370667
lux-ferre <milne_christopher@hotmail.com>
2024-07-18 15:53:37 +0100

    ssl mail config in config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') == "True"
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') == "True"
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
