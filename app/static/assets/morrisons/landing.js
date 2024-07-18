156af2fd460a9389a84003b5483ec431a363a0df
lux-ferre <milne_christopher@hotmail.com>
2024-07-18 21:01:13 +0100

    rudimentary password for access to guide book

--- app/static/assets/morrisons/landing.js
+++ app/static/assets/morrisons/landing.js
@@ -8,8 +8,23 @@ $("#recs_card").on("click", e =>{
     window.location.href = $("#flask_data").data("recs")
 })
 $("#guide_card").on("click", e =>{
-    window.location.href = "https://tinyurl.com/2241guide"
+    $("#password_modal").modal("show")
 })
+
+$("#password_input_submit").on("click", e =>{
+    const input = $("#password_input")
+    const pass = input.val()
+    input.val("")
+
+    $("#password_modal").modal("hide")
+
+    if (pass === "2241"){
+        window.location.href = "https://tinyurl.com/2241guide"
+    } else {
+        window.alert("Password incorrect!")
+    }
+})
+
 $("#barrs_card").on("click", e =>{
     window.location.href = "https://tinyurl.com/4davs5a4"
 })
