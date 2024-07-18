d31eb47f81337f30a2a48c3c3553b51421e5361e
lux-ferre <milne_christopher@hotmail.com>
2024-07-18 20:52:27 +0100

    morrisons landing page js split into module

---
+++ app/static/assets/morrisons/landing.js
@@ -0,0 +1,15 @@
+$("#mylearning_card").on("click", e =>{
+    window.location.href = "https://morrisons.kallidus-suite.com/external"
+})
+$("#verisae_card").on("click", e =>{
+    window.location.href = "https://eam.verisae.co.uk/"
+})
+$("#recs_card").on("click", e =>{
+    window.location.href = $("#flask_data").data("recs")
+})
+$("#guide_card").on("click", e =>{
+    window.location.href = "https://tinyurl.com/2241guide"
+})
+$("#barrs_card").on("click", e =>{
+    window.location.href = "https://tinyurl.com/4davs5a4"
+})
