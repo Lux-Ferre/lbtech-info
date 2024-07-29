0a65c36da18dd41280bae5bc29c4e50584e56435
lux-ferre <milne_christopher@hotmail.com>
2024-07-29 21:07:03 +0100

    barrs order support to generate pdf instead of csv

--- app/static/assets/morrisons/barrs/barrs_order.js
+++ app/static/assets/morrisons/barrs/barrs_order.js
@@ -1,124 +1,167 @@
-class Barrs{
-	constructor(){
-		this.products = {}
-		this.order_size =0
+class Barrs {
+    constructor() {
+        this.products = {}
+        this.order_size = 0
 
-		this.load_products()
-	}
+        this.load_products()
+    }
 
-	load_products(){
-		import("./barrs_details.js").then(mod=>{
-			this.products = JSON.parse(mod.barrs_details)
-			for (const idx of Object.keys(this.products)){
-				this.products[idx].qty = 0
-				this.products[idx].price_marked = this.products[idx].price_marked === "TRUE"
-			}
-			this.populate_table()
-		})
-	}
+    load_products() {
+        import("./barrs_details.js").then(mod => {
+            this.products = JSON.parse(mod.barrs_details)
+            for (const idx of Object.keys(this.products)) {
+                this.products[idx].qty = 0
+                this.products[idx].price_marked = this.products[idx].price_marked === "TRUE"
+            }
+            this.populate_table()
+        })
+    }
 
-	populate_table(){
-		const table = $("#table_body")
-		const template = $("#row_template")
+    populate_table() {
+        const table = $("#table_body")
+        const template = $("#row_template")
 
-		for (const [idx, product] of Object.entries(this.products)){
-			let new_row = template.prop("content").cloneNode(true)
+        for (const [idx, product] of Object.entries(this.products)) {
+            let new_row = template.prop("content").cloneNode(true)
 
-			$(".cell_description", new_row).text(product.simplified_description)
-			$(".cell_size", new_row).text(product.size)
-			$(".cell_pmp", new_row).text(product.price_marked ? "✔️":"❌")
-			$("tr", new_row).attr("data-idx", idx)
-			table.append(new_row)
-		}
-	}
+            $(".cell_description", new_row).text(product.simplified_description)
+            $(".cell_size", new_row).text(product.size)
+            $(".cell_pmp", new_row).text(product.price_marked ? "✔️" : "❌")
+            $("tr", new_row).attr("data-idx", idx)
+            table.append(new_row)
+        }
+    }
 
-	update_table(){
-		const pmp_unfiltered = !($("#input_pmp").is(':checked'))
-		const size_value = $("#input_size").find(":selected").val()
-		const search_value = $("#input_search").val().toLowerCase()
-		$("tr", "#table_body").each((i, e)=>{
-			let visible = true
-			const ele = $(e)
-			const idx = $(ele).data("idx")
+    update_table() {
+        const pmp_unfiltered = !($("#input_pmp").is(':checked'))
+        const size_value = $("#input_size").find(":selected").val()
+        const search_value = $("#input_search").val().toLowerCase()
+        $("tr", "#table_body").each((i, e) => {
+            let visible = true
+            const ele = $(e)
+            const idx = $(ele).data("idx")
 
-			if(!(pmp_unfiltered || this.products[idx].price_marked)){
-				visible = false
-			}
+            if (!(pmp_unfiltered || this.products[idx].price_marked)) {
+                visible = false
+            }
 
-			if(!(size_value === "any" || size_value === this.products[idx].size)){
-				visible = false
-			}
+            if (!(size_value === "any" || size_value === this.products[idx].size)) {
+                visible = false
+            }
 
-			if(!(search_value === "" || this.products[idx].simplified_description.toLowerCase().includes(search_value))){
-				visible = false
-			}
+            if (!(search_value === "" || this.products[idx].simplified_description.toLowerCase().includes(search_value))) {
+                visible = false
+            }
 
-			if(visible){
-				ele.removeClass("d-none")
-			} else {
-				ele.addClass("d-none")
-			}
-		})
-	}
+            if (visible) {
+                ele.removeClass("d-none")
+            } else {
+                ele.addClass("d-none")
+            }
+        })
+    }
 
-	create_csv(){
-		const headers = []
-		$(".form-check-input", $("#save_modal")).each((i, e)=>{
-			const ele = $(e)
-			if(ele.is(":checked")){
-				headers.push(ele.data("col"))
-			}
-		})
+    create_csv() {
+        const headers = []
+        $(".form-check-input", $("#save_modal")).each((i, e) => {
+            const ele = $(e)
+            if (ele.is(":checked")) {
+                headers.push(ele.data("col"))
+            }
+        })
 
-		if(headers.length < 1){return}
+        if (headers.length < 1) {
+            return
+        }
 
-		let csv_string = headers.join(",") + "\n"
+        let csv_string = headers.join(",") + "\n"
 
-		for(const product of Object.values(barrs.products)){
-			if(product.qty>0){
-				const data = []
-				headers.forEach(col=>{
-					data.push(product[col])
-				})
+        for (const product of Object.values(barrs.products)) {
+            if (product.qty > 0) {
+                const data = []
+                headers.forEach(col => {
+                    data.push(product[col])
+                })
 
-				csv_string += (data.join(",") + "\n")
-			}
-		}
+                csv_string += (data.join(",") + "\n")
+            }
+        }
 
-		let file = new File([csv_string], "order.csv", { type: "text/csv;charset=utf-8" })
-		saveAs(file)
-	}
+        let file = new File([csv_string], "order.csv", {type: "text/csv;charset=utf-8"})
+        saveAs(file)
+    }
+
+    create_pdf() {
+        const headers = []
+        $(".form-check-input", $("#save_modal")).each((i, e) => {
+            const ele = $(e)
+            if (ele.is(":checked")) {
+                headers.push(ele.data("col"))
+            }
+        })
+
+        if (headers.length < 1) {
+            return
+        }
+
+        const {jsPDF} = window.jspdf
+        const doc = new jsPDF("landscape")
+        doc.text("Barr's Order", 10, 10)
+
+        const table_data = []
+        for (const product of Object.values(barrs.products)) {
+            if (product.qty > 0) {
+                const row_data = [];
+                headers.forEach(col => {
+                    row_data.push(product[col]);
+                });
+                table_data.push(row_data);
+            }
+        }
+
+        doc.autoTable({
+            head: [headers],
+            body: table_data,
+        })
+
+
+        doc.save('order.pdf');
+    }
 }
 
 window.barrs = new Barrs()
 
-$("#input_pmp").on("input", e=>{
-	barrs.update_table()
+$("#input_pmp").on("input", e => {
+    barrs.update_table()
 })
 
-$("#input_size").on("input", e=>{
-	barrs.update_table()
+$("#input_size").on("input", e => {
+    barrs.update_table()
 })
 
-$("#input_search").on("input", e=>{
-	barrs.update_table()
+$("#input_search").on("input", e => {
+    barrs.update_table()
 })
 
-$("#input_save").on("click", e=>{
-	$("#save_order_size").text(`Order size: ${barrs.order_size}`)
-	$("#save_modal").modal("show")
+$("#input_save").on("click", e => {
+    $("#save_order_size").text(`Order size: ${barrs.order_size}`)
+    $("#save_modal").modal("show")
 })
 
-$("#save_save").on("click", e=>{
-	barrs.create_csv()
+$("#save_csv").on("click", e => {
+    barrs.create_csv()
 })
 
-$("#table_body").on('input', '.input_qty', e=>{
-	const qty = parseInt($(e.target).val())
-	const idx = $(e.target).parent().parent().data("idx")
-	barrs.products[idx].qty = qty
-	barrs.order_size = 0
-	Object.values(barrs.products).forEach(product=>{
-		barrs.order_size += product.qty
-	})
-});
+$("#save_pdf").on("click", e => {
+    barrs.create_pdf()
+})
+
+$("#table_body").on('input', '.input_qty', e => {
+    const qty = parseInt($(e.target).val())
+    const idx = $(e.target).parent().parent().data("idx")
+    barrs.products[idx].qty = qty
+    barrs.order_size = 0
+    Object.values(barrs.products).forEach(product => {
+        barrs.order_size += product.qty
+    })
+})
