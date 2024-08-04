daeb31054b14439cf6636db4c70907686fd46252
lux-ferre <milne_christopher@hotmail.com>
2024-08-04 16:37:22 +0100

    moved templates to match view/asset dir structure

--- app/views/main.py
+++ app/views/main.py
@@ -7,12 +7,12 @@ from app.email import send_contact_email
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("main/index.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("main/about.html")
 
 
 @app.route("/contact", methods=["GET", "POST"])
@@ -21,14 +21,14 @@ def contact():
     if form.validate_on_submit():
         send_contact_email(contact=form.email.data, input_message=form.message.data)
         return redirect(url_for('contact_success'))
-    return render_template("contact.html", form=form)
+    return render_template("main/contact.html", form=form)
 
 
 @app.route("/contact/success")
 def contact_success():
-    return render_template("contact_success.html")
+    return render_template("main/contact_success.html")
 
 
 @app.route("/books")
 def books():
-    return render_template("books.html")
+    return render_template("main/books.html")
